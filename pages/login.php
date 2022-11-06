128f781f089872a7d3570b5debef4914de1a8f25
Laurence Cadag <laurencecads1@gmail.com>
2022-11-06 11:39:13 +0800

    Commit 2: Created Login.php

---
+++ pages/login.php
@@ -0,0 +1,32 @@
+<?php include '../assets/includes/includes-head.php'; ?>
+<style> <?php include_once '../assets/styles/default.css'; ?></style>
+<title>Blog.net</title>
+</head>
+<body>
+  <div class="login-container">
+    <div class="project-description">
+      <h1>Blog.net</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor fugiat vitae temporibus cumque ipsam libero doloribus, et pariatur. Laborum atque consequatur nesciunt delectus corrupti quae saepe officiis sint placeat? Suscipit.
+      </p>
+      <a href="" class="project-cta">Learn more &rarr;</a>
+    </div>
+    <div class="project-login">
+      <h2>Login</h2>
+      <form action="" autocomplete="off" id="login-form">
+        <label for="userName">Username</label>
+        <input type="text" name="userName" id="userName" required>
+        <label for="passWord">Password</label>
+        <input type="text" name="passWord" id="passWord" required>
+        <div class="project-signup">
+          <button type="submit" id="login-btn">Login</button>
+          <p class="project-new">No account yet? Sign up <a href="" class="project-cta-sign">here</a></p>
+          
+        </div>
+      </form>
+
+    </div>
+  </div>
+
+</body>
+</html>
